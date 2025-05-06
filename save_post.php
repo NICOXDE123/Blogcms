dfb3073c8fdc101ec0bdb2419ad3a06e31a46ca8
NICOXDE123 <nhuenchual2024@alu.uct.cl>
2025-05-06 17:02:53 -0400

    commit 1
    trabajo casi

---
+++ save_post.php
@@ -0,0 +1,18 @@
+<?php
+require 'db.php';
+
+$title = $_POST['title'];
+$content = $_POST['content'];
+$imag = $_POST['image']
+
+
+$stmt = $conn->prepare("INSERT INTO posts (title, content) VALUES (?, ?)");
+$stmt->bind_param("ss", $title, $content);
+$stmt->execute();
+
+$stmt->close();
+$conn->close();
+
+header("Location: posts.php");
+exit();
+?>
